886f75478e7ed64a1003deb826e8c7c8fcfd7ee0
viral712 <viral712@gmail.com>
2018-08-21 17:26:52 +0530

    Second commit

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,23 @@
 pragma solidity ^0.4.24;
 
 contract Election{
-	//constructor
-	string public candidate;
+	struct Candidate{
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	mapping(uint => Candidate) public candidates;
+
+	uint public candidatesCount;
+
 	constructor() public {
-		candidate = "Candidate 1";
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate (string _name) private{
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name,0);
 	}
 }

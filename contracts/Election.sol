0257fc21ec8086896288756c847b47270d8dd9c1
viral712 <viral712@gmail.com>
2018-08-29 23:07:22 +0530

    whatever

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,7 @@ contract Election{
 	}
 
 	mapping(uint => Candidate) public candidates;
+	mapping(address => bool) public voters;
 
 	uint public candidatesCount;
 
@@ -19,5 +20,13 @@ contract Election{
 	function addCandidate (string _name) private{
 		candidatesCount++;
 		candidates[candidatesCount] = Candidate(candidatesCount, _name,0);
+	}
+
+	function vote (uint _candidateId) public {
+		require(!voters[msg.sender]);
+		require(_candidateId>0 && _candidateId <= candidatesCount);
+		voters[msg.sender] = true;
+		candidates[_candidateId].voteCount++;
+
 	}
 }
--- test/election.js
+++ test/election.js
@@ -24,4 +24,60 @@ contract("Election", function(accounts){
             assert.equal(candidate[2], 0, "contains the correct votes count");
         });
     });
+
+    it("allows a voter to cast a vote", function(){
+        return Election.deployed().then(function(instance){
+            electionInstance = instance;
+            candidateId = 1;
+            return electionInstance.vote(candidateId, { from: accounts[0] });
+        }).then(function (receipt){
+            return electionInstance.voters(accounts[0]);
+        }).then(function (voted){
+            assert(voted, "the voter was marked as voted");
+            return electionInstance.candidates(candidateId);
+        }).then(function(candidate){
+            var voteCount = candidate[2];
+            assert.equal(voteCount, 1, "increments the candidates vote count by 1");
+        });
+    });
+
+    it("throws an exception for invalid candidate", function(){
+        return Election.deployed().then(function(instance){
+            electionInstance = instance;
+            return electionInstance.vote(99, {from: accounts[1]});
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert')>=0, "error message must contain revert");
+            return electionInstance.candidates(1);
+        }).then(function(candidate1){
+            var voteCount = candidate1[2];
+            assert.equal(voteCount, 1, "vote count did not change for candidate1");
+            return electionInstance.candidates(2);
+        }).then( function(candidate2){
+            var voteCount = candidate2[2];
+            assert.equal(voteCount, 0, "vote count did not change for candidate2");
+        });
+    });
+
+    it("throws an exception for voting twice", function(){
+        return Election.deployed().then(function(instance){
+            electionInstance = instance;
+            candidateId = 2;
+            electionInstance.vote(candidateId, { from: accounts[1] });
+            return electionInstance.candidates(candidateId);
+        }).then(function(candidate2){
+            var voteCount = candidate2[2];
+            assert.equal(voteCount, 1, "vote count for candidate2 is 1");
+            return electionInstance.vote(candidateId, { from: accounts[1] });
+        }).then(assert.fail).catch(function(error){
+            assert(error.message.indexOf('revert')>=0, "error message must contain revert");
+            return electionInstance.candidates(1);
+        }).then(function(candidate1){
+            var voteCount = candidate1[2];
+            assert.equal(voteCount, 1, "vote count did not change for candidate1");
+            return electionInstance.candidates(2);
+        }).then( function(candidate2){
+            var voteCount = candidate2[2];
+            assert.equal(voteCount, 1, "vote count did not change for candidate2");
+        });
+    });
 });
